6a77416d098606dbea19aac343e03615905cc1e3
Man <manggman24@gmail.com>
2026-03-28 10:23:24 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,19 @@
-namespace myRobot {
+//% color="#0fbc11" icon="\uf018"
+namespace MyRobot {
+
+    // (โค้ดเดินหน้าของแมนที่มีอยู่แล้ว)
     //% block="เดินหน้า"
     export function moveForward(): void {
         basic.showIcon(IconNames.Happy)
+    }
+
+    /**
+     * บล็อกสำหรับวาดหน้าตาลงบนจอ LED
+     */
+    //% block="แสดงหน้าตา %img"
+    //% img.shadow="device_build_image"
+    export function showCustomFace(img: Image): void {
+        // คำสั่งนี้จะเอาภาพที่แมนวาดไปแสดงบนจอครับ
+        img.showImage(0)
     }
 }
